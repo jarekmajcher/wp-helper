2f678c274050cfe1f65c476d1f3225a98fb10fb1
jaroslawmajcher <jarek@majcher.me>
2025-12-17 10:38:17 +0100

    fix: smtp config

--- src/Wordpress/Smtp.php
+++ src/Wordpress/Smtp.php
@@ -12,14 +12,6 @@ class Smtp extends \Jm\WpHelper\WpHelper {
     public function __construct() {
         parent::__construct();
 
-        add_action('phpmailer_init', [$this, 'smtp_settings']);
-    }
-
-    /**
-     * @param $phpmailer
-     * @return void
-     */
-    public function smtp_settings($phpmailer) : void {
         if(
             defined('JM_SMTP_HOST') &&
             defined('JM_SMTP_PORT') &&
@@ -29,19 +21,28 @@ class Smtp extends \Jm\WpHelper\WpHelper {
             defined('JM_SMTP_FROM') &&
             defined('JM_SMTP_FROM_NAME')
         ) {
-            if(!is_object($phpmailer)) {
-                $phpmailer = (object)$phpmailer;
-            }
-
-            $phpmailer->isSMTP();
-            $phpmailer->SMTPAuth = true;
-            $phpmailer->Host = JM_SMTP_HOST;
-            $phpmailer->Port = JM_SMTP_PORT;
-            $phpmailer->Username = JM_SMTP_USERNAME;
-            $phpmailer->Password = JM_SMTP_PASSWORD;
-            $phpmailer->SMTPSecure = JM_SMTP_SECURE;
-            $phpmailer->From = JM_SMTP_FROM;
-            $phpmailer->FromName = JM_SMTP_FROM_NAME;
+            add_action('phpmailer_init', [$this, 'smtp_settings']);
+            add_filter('wp_mail_from', fn() => JM_SMTP_FROM);
+            add_filter('wp_mail_from_name', fn() => JM_SMTP_FROM_NAME);
         }
+    }
+
+    /**
+     * @param $phpmailer
+     * @return void
+     */
+    public function smtp_settings($phpmailer) : void {
+        if(!is_object($phpmailer)) {
+            $phpmailer = (object)$phpmailer;
+        }
+
+        $phpmailer->isSMTP();
+        $phpmailer->SMTPAuth = true;
+        $phpmailer->Host = JM_SMTP_HOST;
+        $phpmailer->Port = JM_SMTP_PORT;
+        $phpmailer->Username = JM_SMTP_USERNAME;
+        $phpmailer->Password = JM_SMTP_PASSWORD;
+        $phpmailer->SMTPSecure = JM_SMTP_SECURE;
+        $phpmailer->setFrom(JM_SMTP_FROM, JM_SMTP_FROM_NAME, false);
     }
 }
